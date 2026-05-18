8ee9b0d53a9830d2b183a090d2829fa1a6f88968
olvchae <eunchae1405@gmail.com>
2026-05-18 17:50:53 +0900

    리액트 버전19 리코일제거, 주스탠드 설치

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
-import { RecoilRoot } from 'recoil'
+
 
 createRoot(document.getElementById('root')).render(
-  <RecoilRoot>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </RecoilRoot>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
--- src/recoil/atoms/playerAtom.jsx
+++ src/recoil/atoms/playerAtom.jsx
@@ -1,8 +1,8 @@
-import { atom } from "recoil";
+import { create } from "zustand";
 
-export const playerState = atom({
+export const playerState = create({
     key: "playerState",
-    
+
     default: {
         username: "user",
         hp: 50,
@@ -34,5 +34,28 @@ export const playerState = atom({
 
         buffs: [],
         debuffs: [],
+
+        getFinalAttack: () => {
+            const player = get(playerState);    // player의 정보를 가져옴 (체력 공격력 버프 등)
+            let finalAttack = player.attack;
+
+            player.buffs.forEach(buff => {          // player 기본 공격력 * 버프수치 +
+                if (buff.type === "attackUp") {
+                    finalAttack += player.attack * buff.value;
+                }
+            });
+
+            player.debuffs.forEach(debuff => {          // player 기본 공격력 * 디버프수치 -
+                if (debuff.type === "attackDown") {
+                    finalAttack -= player.attack * debuff.value;
+                }
+            });
+
+            if (player.corruption >= 40) {
+                finalAttack = Math.floor(finalAttack * 1.2);
+            }
+
+            return finalAttack;
+        }
     }
 })
